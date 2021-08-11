13cc2b2e81ae37032cefc949ef308bca992bc6cd
Rin Oliver <all.about.them.words@gmail.com>
2021-08-11 17:34:11 -0500

    update jest config

--- jest.config.js
+++ jest.config.js
@@ -1,15 +1,12 @@
-// jest.config.js
-// Sync object
-/** @type {import('@jest/types').Config.InitialOptions} */
-const config = {
-    verbose: true,
-  };
-  transformIgnorePatterns: ["/node_modules/(?!vue-awesome)"],
-  module.exports = config;
-  
-  // Or async function
-  module.exports = async () => {
-    return {
-      verbose: true,
-    };
-  };
+module.exports = {
+    preset: 'react-native',
+    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+    transformIgnorePatterns: [
+      "node_modules/(?!(react-native"
+        + "|react-navigation-tabs"
+        + "|react-native-splash-screen"
+        + "|react-native-screens"
+        + "|react-native-reanimated"
+      + ")/)",
+    ],
+  }
