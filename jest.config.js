033eaa871f402a9ce0209661734a83ce852730d8
Rin Oliver <all.about.them.words@gmail.com>
2021-08-12 09:25:02 -0500

    update syntax transform ignore patterns

--- jest.config.js
+++ jest.config.js
@@ -1,15 +1,10 @@
 module.exports = {
     preset: 'react-native',
     moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-    transformIgnorePatterns: [
-        + "|react-navigation-tabs"
-        + "|react-native-splash-screen"
-        + "|react-native-screens"
-        + "|react-native-reanimated"
-      + ")/)"
-      + "|react-native"
-      + "|node-modules"
-    ],
+    "transformIgnorePatterns": [
+        "node_modules/(?!(@react-native|react-native)/)",
+      ]
+    ,
   }
 "transform"; {
     "^.+\\.tsx?$"; 
