dc07ba8b5426c9eab61923d0e4d3297315589a2a
Rin Oliver <all.about.them.words@gmail.com>
2021-08-12 09:43:00 -0500

    update jest.config

--- jest.config.js
+++ jest.config.js
@@ -1,20 +1,6 @@
 module.exports = {
     preset: 'react-native',
-    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-    "transformIgnorePatterns": [
-        "node_modules/(?!(@react-native|react-native)/)",
-      ]
-    
-  }
-"transform"; {
-    "^.+\\.tsx?$"; 
-    "^.+\\.(js|jsx|mjs)?$"; 
-    "^.+\\.ts-jest";
-"moduleFileExtensions"; [
-    "ts",
-    "tsx",
-    "js",
-    "jsx",
-    "json",
-    "node"
-]}
+    transform: {
+      '^.+\\.js$': require.resolve('react-native/jest/preprocessor.js'),
+    },
+  };
