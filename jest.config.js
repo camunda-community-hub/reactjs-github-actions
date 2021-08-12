fdea37acb1a83e611a4475ef35ae53bacffed6cc
Rin Oliver <all.about.them.words@gmail.com>
2021-08-12 09:12:36 -0500

    update jest config

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,6 @@ module.exports = {
     preset: 'react-native',
     moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
     transformIgnorePatterns: [
-      "node_modules/(?!(react-native"
         + "|react-navigation-tabs"
         + "|react-native-splash-screen"
         + "|react-native-screens"
