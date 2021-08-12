d453287f03b266b1185a898ff5a71e5860fa462a
Rin Oliver <all.about.them.words@gmail.com>
2021-08-12 08:38:04 -0500

    update jest.config

--- jest.config.js
+++ jest.config.js
@@ -7,15 +7,17 @@ module.exports = {
         + "|react-native-splash-screen"
         + "|react-native-screens"
         + "|react-native-reanimated"
+        + "|react-native"
+        + "|jest-runtime"
       + ")/)",
     ],
   }
   "testEnvironment"; "node",
-"transformIgnorePatterns"; ["/node_modules/"],
+"transformIgnorePatterns"; ['/node_modules/(?!${jest-runtime})', '/node_modules/(?!${react-native})']
 // transformIgnorePatterns: [`/node_modules/(?!${esModules})`],
 "transform"; {
     "^.+\\.tsx?$"; "ts-jest",
-    "^.+\\.(js|jsx|mjs)?$"; "babel-jest"
+    "^.+\\.(js|jsx|mjs)?$"; "babel-jest";
 "moduleFileExtensions"; [
     "ts",
     "tsx",
