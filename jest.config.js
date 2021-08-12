0e4df57a2cc8050c335f3ee54cf65e56830cb8aa
Rin Oliver <all.about.them.words@gmail.com>
2021-08-12 13:30:23 -0500

    update jest config

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,13 @@
-"modulePaths"; [
-    "<rootDir>"
-  ],
-testEnvironment; 'jsdom'
-module.exports = {
-    preset: 'react-native',
-    transform: {
-      '^.+\\.js$': require.resolve('react-native/jest/preprocessor.js'),
-      ".+\\.(css|styl|less|sass|scss)$": "jest-transform-css"
-    },
-  };
+process.env.BABEL_ENV = 'test';
+process.env.NODE_ENV = 'test';
+process.env.PUBLIC_URL = '';
+require('react-scripts/config/env');
+
+const path = require('path');
+const createJestConfig = require('react-scripts/scripts/utils/createJestConfig');
+
+module.exports = createJestConfig(
+  relativePath => require.resolve(path.join('react-scripts', relativePath)),
+  __dirname, // given that Jest config is in project root
+  false
+);
